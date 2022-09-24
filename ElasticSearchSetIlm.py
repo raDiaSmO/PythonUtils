6d54f52c8cba93edf92ca351e588993d0da882f5
raDiaSmO <83611528+raDiaSmO@users.noreply.github.com>
2022-09-23 23:35:17 -0400

    Update ElasticSearchSetIlm.py

--- ElasticSearchSetIlm.py
+++ ElasticSearchSetIlm.py
@@ -10,7 +10,7 @@ def elk_set_ilm():
     port = sys.argv[2]
     protocol = sys.argv[3]
     f_host = f'{protocol}://{ip}:{port}'
-    url = re.sub('\?|\!|\'|\n|\'|\;', '', f_host)
+    url = re.sub('\?|\!|\'|\n|\;', '', f_host)
     beats = ['auditbeat','filebeat','metricbeat','packetbeat']
     
     header = {
@@ -21,16 +21,35 @@ def elk_set_ilm():
       "policy": {
         "phases": {
           "hot": {
+            "min_age": "0ms",
             "actions": {
               "rollover": {
-                "max_primary_shard_size": "50GB"
+                "max_primary_shard_size": "50gb"
+              }
+            }
+          },
+          "warm": {
+            "min_age": "3d",
+            "actions": {
+              "set_priority": {
+                "priority": 50
+              }
+            }
+          },
+          "cold": {
+            "min_age": "7d",
+            "actions": {
+              "set_priority": {
+                "priority": 0
               }
             }
           },
           "delete": {
             "min_age": "30d",
             "actions": {
-              "delete": {} 
+              "delete": {
+                "delete_searchable_snapshot": True
+              }
             }
           }
         }
@@ -40,8 +59,8 @@ def elk_set_ilm():
     for beat in beats:
         
         try:
-            f_url = f'{url}/_ilm/policy/beat'
-            r = requests.put(f_url, data=config, headers=header)
+            f_url = f'{url}/_ilm/policy/{beat}'
+            r = requests.put(f_url, json=config, headers=header)
             print(r,'\n',r.content)
 
         except:
